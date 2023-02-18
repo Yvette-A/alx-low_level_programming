2593113d278dea325afdb96828a7219a63b54594
Yvette Achieng <achiengyvette@gmail.com>
2023-02-17 23:47:07 -0800

    add a file last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,10 +11,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n < 6 && n != 0)
+	/* variable m gets the modular division which gives us the last digit of n */
+	if ((n % 10) < 6 && (n % 10) != 0)
 		printf("Last digit of %d is less than 6 and not 0\n", n);
-	else if (n > 5)
+	else if ((n % 10) > 5)
 		printf("Last digit of %d is greater than 5\n", n);
 	else
 		printf("Last digit of %d is 0\n", n);
