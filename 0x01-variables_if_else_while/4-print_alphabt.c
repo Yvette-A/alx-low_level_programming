bc12f323f1f77f53f50355dd5d21b1adbd5ef4ee
Yvette Achieng <achiengyvette@gmail.com>
2023-02-19 00:55:00 -0800

    file 4 added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+/**
+ * main - entry point
+ * Return: always 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while
+		(ch <= 'z');
+	{
+		if ((ch != 'e' && ch != 'q') && ch  <= 'z')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
