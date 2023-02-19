c02e81677e7e4cdcc8f9e69220a1857d7df3d52a
Yvette Achieng <achiengyvette@gmail.com>
2023-02-19 04:57:49 -0800

    add file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10)
+		{
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
