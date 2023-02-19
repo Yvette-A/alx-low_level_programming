1147ee70a55f5feba4715f196ea8c052c370a7b5
Yvette Achieng <achiengyvette@gmail.com>
2023-02-19 05:09:03 -0800

    file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/**
+ * main - entry point
+ * Return: always 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while
+		(ch >= 'a')
+		{
+			putchar(ch);
+			ch--;
+		}
+	putchar('\n');
+	return (0);
+}
