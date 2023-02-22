208b129a8b2d3e9716ed94388b07c241a81905ea
Yvette Achieng <achiengyvette@gmail.com>
2023-02-22 04:13:20 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,13 @@
 /**
  * _abs - computes the absolute value of and integer
  * @x: integer to be tested
- * Return: the absolute value 
+ * Return: the absolute value
  */
 #include "main.h"
 int _abs(int x)
 {
-	if ( x < 0)
-	{
-		_putchar(((x * - 1) % 10) - '0');
-	}
-	else if
-		(x > 0)
-		{
-			_putchar((x % 10) + '0');
-		}
+	if (x >= 0)
+		return (x);
 	else
-	{
-		_putchar('0');
-	}
-	return (0);
+		return (-x);
 }
