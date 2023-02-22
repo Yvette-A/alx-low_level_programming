e9a4c18bc6d08b6e76810a41dc428501998332a6
Yvette Achieng <achiengyvette@gmail.com>
2023-02-22 03:16:11 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - add two integers
+ * @a: inpute integer 1
+ * @b: input integer 2
+ * Return: Added number
+ */
+#include "main.h"
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	return (x);
+}
