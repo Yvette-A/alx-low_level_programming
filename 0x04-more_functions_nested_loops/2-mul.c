80d7b7eba30aba070632e60f3b20eece0bf0b904
Yvette Achieng <achiengyvette@gmail.com>
2023-02-24 00:00:40 -0800

    multiplication of two munbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number to be input
+ * @b: second number to be input
+ * Return: the multiplied value
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
