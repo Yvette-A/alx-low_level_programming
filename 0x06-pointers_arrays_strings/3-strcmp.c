7f53545de9da1579ae72ef72791e7f1cff0e244b
Yvette Achieng <achiengyvette@gmail.com>
2023-03-01 07:37:24 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int index, y;
-	/* initializing index */
-	index = 0;
-	y = s1[index] - s2[index];
-	for (index = 0; s1[index] == s2[index]; index++)
-		if ((s1[index] == s2[index]) && (s1[index] == '\0' || s2[index] == '\0'))
-			return (0);
-	return (y);
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
