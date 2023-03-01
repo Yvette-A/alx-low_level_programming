c76612255bfa60fc06450dc52467e46b034cdbad
Yvette Achieng <achiengyvette@gmail.com>
2023-03-01 06:52:46 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strngs
+ * @s1: first string
+ * @s2: second string
+ * Return: the value of comparisong
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int index, y;
+	/* initializing index */
+	index = 0;
+	y = s1[index] - s2[index];
+	for (index = 0; s1[index] == s2[index]; index++)
+		if (s1[index] == '\0')
+			return (0);
+	return (y);
+}
