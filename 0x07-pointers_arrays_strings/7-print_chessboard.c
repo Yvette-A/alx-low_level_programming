fef15f83c15919fb17e51766cb4d809e25d71cf5
Yvette Achieng <achiengyvette@gmail.com>
2023-03-06 06:32:58 -0800

    print chess board prototype

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#inlcude "main.h"
+
+/**
+ * print_chessboard - prints the chess board
+ * @a: the chess board to be printed
+ * Return: always 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int indx1, indx2;
+
+	for (indx1 = 0; a[indx1][7]; indx1++)
+	{
+		for (indx2 = 0; indx2 < 8; indx2++)
+			_putchar(a[indx1][indx2]);
+
+		_putchar('\n');
+	}
+}
