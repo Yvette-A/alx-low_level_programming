87c0d325184d350f1bd4577517ce8a8762fa4da1
Yvette Achieng <achiengyvette@gmail.com>
2023-03-06 01:04:56 -0800

    prototype of the standard libraby memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to a location
+ * @b: a constant to fill the memory
+ * @n: the first number of bites of memory area pointed
+ * Return: a pointer to the memory s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *y;
+	unsigned int index;
+
+	y = s;
+	index = 0;
+	/* to get the number of n bytes */
+	for (index = 0; index < n; index++)
+		*y++ = b;
+	return (y);
+}
