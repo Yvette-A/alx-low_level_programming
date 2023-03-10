88a22d4ee8e73336e14918480ac826362c6878d1
Yvette Achieng <achiengyvette@gmail.com>
2023-03-10 09:48:34 -0800

    script that creates a static library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _atoi - changes ascii value to integer
+ * @s: character to change to integer
+ * Return: the integer value
+ */
+
+int _atoi(char *s)
+{
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies s string from source to destination
+ * @dest: destination of the string
+ * @src: the source of the string
+ * Return: the copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+}
