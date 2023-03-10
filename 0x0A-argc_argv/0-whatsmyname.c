e29a02bce0878b9b7ec9433547e2e643a059aa44
Yvette Achieng <achiengyvette@gmail.com>
2023-03-10 00:49:09 -0800

    a program to change name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its own name
+ * @argc: the number of arguments passed
+ * @argv: the pointer to the array of arguments
+ * Return: the name of the arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks about commanand line arguments in C progrmming
