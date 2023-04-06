7385cc0e056be8043403126f2a300cac2492f607
Yvette Achieng <achiengyvette@gmail.com>
2023-04-06 07:38:18 -0700

    adding new files and README

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: pointer to a string of 1s and 0s
+ * Return: a pointer to converted unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int mul = 1, length = 1, i;
+	unsigned int num = 0;
+	
+	/*iterating through the string to find the length */
+	for (length = 1; b[length]; length++)
+		if (b[length] != '0' && b[length] != '1')
+			return (0);
+	/* iterating backward to find the position of 1 */
+	for (i = (length - 1); i >= 0; i--)
+	{
+		if (b[i] == '1')
+			num +=  mul;
+		mul *= 2;
+	}
+	return (num);
+}
