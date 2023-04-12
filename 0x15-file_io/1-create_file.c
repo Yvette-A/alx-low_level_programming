47a8abf0b09e75bf8954f771a8c36bf69162abdc
Yvette Achieng <achiengyvette@gmail.com>
2023-04-12 06:39:44 -0700

    adds file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and trancates one that already exists
+ * @filename: the name of the file to be created if it doesnot exist
+ * @text_content: the content to be written in the file created
+ * Return: 1 if the creation was successfull
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len;
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		for (len = 0; text_content[len];)
+			len++;
+	fd = creat(filename, 0600);
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
