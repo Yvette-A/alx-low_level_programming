6220d62e561ab8f9c1e8f33edf62b93adf61a682
Yvette Achieng <achiengyvette@gmail.com>
2023-04-12 06:54:40 -0700

    minor changes made to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
-		for (len = 0; text_content[len];)
+	for (len = 0; text_content[len];)
 			len++;
 	fd = creat(filename, 0600);
 	w = write(fd, text_content, len);
