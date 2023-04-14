0b84e463c4fcfd43af25af4450a3b025f394bd57
Yvette Achieng <achiengyvette@gmail.com>
2023-04-13 20:13:48 -0700

    changes to file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,12 +14,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = creat(filename, 0600);
-	for (len = 0; text_content[len];)
-		len++;
-	w = write(fd, text_content, len);
-	if (fd == -1 || w == -1)
-	{
+	if (fd == -1)
 		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
 	}
 	close(fd);
 	return (1);
