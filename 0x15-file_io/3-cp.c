65927b65f5790f7c630b1979ff1a7677ffb6fec4
Yvette Achieng <achiengyvette@gmail.com>
2023-04-14 00:16:40 -0700

    minor changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,11 +37,6 @@ int main(int argc, char **argv)
 	}
 	while ((n = read(f1, buff, BUFSIZE)) > 0)
 		m = write(f2, buff, n);
-	if (m != n)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't wrie to file %s\n", argv[2]);
-		exit(99);
-	}
 	free(buff);
 	close_file(f1);
 	close_file(f2);
