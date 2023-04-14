4e3b49a01d2fc7da72ee808d3f7498d07c728524
Yvette Achieng <achiengyvette@gmail.com>
2023-04-13 19:59:44 -0700

    changes to file 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,11 +14,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (1);
 	for (len = 0; text_content[len];)
 		len++;
-	w = write(fd, text_content, len);
-	if (fd == -1 || w == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
