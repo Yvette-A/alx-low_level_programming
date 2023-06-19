8c9b208bd68ae5197d843334b9b54e95ad4a9d99
Yvette Achieng <achiengyvette@gmail.com>
2023-06-19 01:57:26 -0700

    adding new file

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/**
+ * add - adds two intergers
+ * @a: the first integer
+ * @b: the second interger
+ * Return: the sum to the two values
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: the first number
+ * @b: the second numbr
+ * Return: the difference between the two numbers
+ */
+int sub(int a, int b)
+{
+	return ((a) - (b));
+}
+/**
+ * mul - multiplies two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the product of the two numbers
+ */
+int mul(int a, int b)
+{
+	return ((a) * (b));
+}
+
+/**
+ * div - divides two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the division of two numbers
+ */
+int div(int a, int b)
+{
+	return ((a) / (b));
+}
+
+/**
+ * mod - returns the modulus of two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the modulus of two numbers
+ */
+int mod(int a, int b)
+{
+	return ((a) % (b));
+}
